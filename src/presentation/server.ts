520b2515b3c670de517570a24ed415c46851b2ef
brayanroa <brayanandresrl@ufps.edu.co>
2025-02-10 10:43:15 -0500

    change in acces key to firebase

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -34,12 +34,12 @@ export class Server {
     }
 
     scheduleCronJobs() {
-        cron.schedule('* * * * * *', async () => {
-            await transactionsRecurring()
-        });
-        cron.schedule('* * * * * *', async () => {
-            await budgetsRecurring()
-        })
+        // cron.schedule('* * * * * *', async () => {
+        //     await transactionsRecurring()
+        // });
+        // cron.schedule('* * * * * *', async () => {
+        //     await budgetsRecurring()
+        // })
         // cron.schedule('* * * * * *', async () => {
         //     await budgetsToBeDeactivated()
         // })
